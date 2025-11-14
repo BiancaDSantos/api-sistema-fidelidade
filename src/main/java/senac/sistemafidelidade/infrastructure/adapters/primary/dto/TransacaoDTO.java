d80b5b2ca7942e0a681ba6b3fbbf0b8f142818f2
Bianca <biancadossantossc@gmail.com>
2025-11-14 17:04:40 -0300

    Corrigida referência de pacote para `TipoTransacao` em `TransacaoDTO` e adicionada validação para `clienteId` em `TransacaoRequest`.

--- src/main/java/senac/sistemafidelidade/infrastructure/adapters/primary/dto/TransacaoDTO.java
+++ src/main/java/senac/sistemafidelidade/infrastructure/adapters/primary/dto/TransacaoDTO.java
@@ -1,18 +1,19 @@
 package senac.sistemafidelidade.infrastructure.adapters.primary.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
-import senac.sistemafidelidade.domain.model.enums.TipoTransacao;
+import senac.sistemafidelidade.domain.enums.TipoTransacao;
 
 import java.time.LocalDateTime;
 
 public interface TransacaoDTO {
 
     record TransacaoRequest(
+
+            @NotNull(message = "ID do cliente é obrigatório")
+            Long clienteId,
 
             @NotNull(message = "Valor da transação é obrigatório")
             @Positive(message = "Valor da transação deve ser positivo")
