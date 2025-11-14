f9eb3bccd9c47e15c0f4286f86097c738d683388
Bianca <biancadossantossc@gmail.com>
2025-11-14 17:02:55 -0300

    Adicionados métodos ao `ContaFidelidadeService` para gerenciamento de contas, incluindo criação, listagem, busca por ID e associação de cliente a empresa.

--- src/main/java/senac/sistemafidelidade/application/service/ContaFidelidadeService.java
+++ src/main/java/senac/sistemafidelidade/application/service/ContaFidelidadeService.java
@@ -1,9 +1,54 @@
 package senac.sistemafidelidade.application.service;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import senac.sistemafidelidade.domain.model.Cliente;
+import senac.sistemafidelidade.domain.model.ContaFidelidade;
+import senac.sistemafidelidade.domain.model.Empresa;
+import senac.sistemafidelidade.domain.repository.ClienteRepository;
+import senac.sistemafidelidade.domain.repository.ContaFidelidadeRepository;
+import senac.sistemafidelidade.domain.repository.EmpresaRepository;
+import senac.sistemafidelidade.infrastructure.adapters.primary.dto.ClienteFidelidadeDTO;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ContaFidelidadeService {
+    
+    private final ContaFidelidadeRepository contaFidelidadeRepository;
+    private final ClienteRepository clienteRepository;
+    private final EmpresaRepository empresaRepository;
+    
+    public ContaFidelidade criarCliente(ClienteFidelidadeDTO.@Valid CriarClienteRequest request) {
+
+        ContaFidelidade conta = request.transformaEmModel();
+        return contaFidelidadeRepository.save(conta);
+    }
+
+    public List<ContaFidelidade> listarClientes() {
+        return contaFidelidadeRepository.findAll();
+    }
+
+    public ContaFidelidade buscarClientePorId(Long id) {
+        return contaFidelidadeRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Cliente não encontrado"));
+    }
+
+    public ContaFidelidade criarContaFidelidade(Long clienteId, Long empresaId) {
+        Cliente cliente = clienteRepository.findById(clienteId)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado para o id: " + clienteId));
+
+        Empresa empresa = empresaRepository.findById(empresaId)
+                .orElseThrow(() -> new IllegalArgumentException("Empresa não encontrada para o id: " + empresaId));
+
+        ContaFidelidade novaConta = ContaFidelidade.builder()
+                .cliente(cliente)
+                .empresa(empresa)
+                .pontos(0)
+                .build();
+
+        return contaFidelidadeRepository.save(novaConta);
+    }
 }
