a1aa464b68e6c00548800ee5a971c2c473f2959e
Bianca <biancadossantossc@gmail.com>
2025-11-16 20:55:47 -0300

    Adicionados endpoints para listar e buscar contas por empresa em `ContaFidelidadeController`, incluindo anotações do Swagger para documentação.

--- src/main/java/senac/sistemafidelidade/infrastructure/adapters/primary/controller/ContaFidelidadeController.java
+++ src/main/java/senac/sistemafidelidade/infrastructure/adapters/primary/controller/ContaFidelidadeController.java
@@ -1,34 +1,63 @@
 package senac.sistemafidelidade.infrastructure.adapters.primary.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import senac.sistemafidelidade.application.service.ContaFidelidadeService;
+import senac.sistemafidelidade.domain.ports.ContaFidelidadeServicePort;
 import senac.sistemafidelidade.infrastructure.adapters.primary.dto.ClienteFidelidadeDTO;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/conta_fidelidade_cliente")
+@RequestMapping("/conta")
 @RequiredArgsConstructor
 public class ContaFidelidadeController {
 
-    private final ContaFidelidadeService contaFidelidadeService;
+    private final ContaFidelidadeServicePort contaFidelidadeService;
+
+    @GetMapping("/empresa/{empresaId}")
+    @Operation(summary = "Lista contas por empresa")
+    public ResponseEntity<List<ClienteFidelidadeDTO.ClienteResponse>> listarContasPorEmpresa(
+            @PathVariable Long empresaId
+    ) {
+        List<ClienteFidelidadeDTO.ClienteResponse> contas =
+                ClienteFidelidadeDTO.ClienteResponse.geraListaDeResponse(
+                        contaFidelidadeService.listarClientesPorEmpresa(empresaId)
+                );
+
+        return ResponseEntity.ok(contas);
+    }
+
+    @GetMapping("/{contaId}/empresa/{empresaId}")
+    @Operation(summary = "Busca conta por ID e empresa")
+    public ResponseEntity<ClienteFidelidadeDTO.ClienteResponse> buscarContaPorIdEEmpresa(
+            @PathVariable Long contaId,
+            @PathVariable Long empresaId
+    ) {
+        return ResponseEntity.ok(
+                new ClienteFidelidadeDTO.ClienteResponse(
+                        contaFidelidadeService.buscarClientePorIdEEmpresa(contaId, empresaId)
+                )
+        );
+    }
 
     @PostMapping
+    @Operation(summary = "Criar nova conta de fidelidade")
     public ResponseEntity<ClienteFidelidadeDTO.ClienteResponse> criarCliente(
             @Valid @RequestBody ClienteFidelidadeDTO.CriarClienteRequest request
     ) {
         return ResponseEntity.ok(
                 new ClienteFidelidadeDTO.ClienteResponse(
-                        contaFidelidadeService.criarCliente(request))
+                        contaFidelidadeService.criarCliente(request)
+                )
         );
     }
 
-    @GetMapping
+    @GetMapping("/listar")
+    @Operation(summary = "Listar todas as contas")
     public ResponseEntity<List<ClienteFidelidadeDTO.ClienteResponse>> listarClientes() {
-
         List<ClienteFidelidadeDTO.ClienteResponse> clientes =
                 ClienteFidelidadeDTO.ClienteResponse
                         .geraListaDeResponse(contaFidelidadeService.listarClientes());
@@ -37,12 +66,14 @@ public class ContaFidelidadeController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar conta por ID")
     public ResponseEntity<ClienteFidelidadeDTO.ClienteResponse> buscarCliente(
             @PathVariable Long id
     ) {
         return ResponseEntity.ok(
                 new ClienteFidelidadeDTO.ClienteResponse(
-                        contaFidelidadeService.buscarClientePorId(id))
+                        contaFidelidadeService.buscarClientePorId(id)
+                )
         );
     }
 }
